c7218d7ba5f7a6393ddc251ad6211df5cb4aa9bb
edsaa <esaavedrac@u.northwestern.edu>
2023-12-29 10:54:55 -0600

    Update styles

--- assets/badges.md
+++ assets/badges.md
@@ -1,11 +1,8 @@
 <div id="fixedbadges" style="position:fixed; bottom: 1%; left:1%;">
-&#x1F4CC; Useful links:
 <br>
-<a href="https://github.com/edsaac/stpyvista"><img alt="stpyvista source code" src="https://img.shields.io/static/v1?label=:&message=Source%20code&color=informational&logo=github"></a>
+<a href="https://github.com/edsaac/stpyvista"><img alt="stpyvista source code" src="https://img.shields.io/static/v1?label=:&message=stpyvista&color=informational&logo=github"></a>
 <br>
-<a href="https://pypi.org/project/stpyvista/"><img alt="stpyvista pypi version" src="https://badgen.net/pypi/v/stpyvista"></a>
+<a href="https://github.com/edsaac/stpyvista"><img alt="geovista source code" src="https://img.shields.io/static/v1?label=:&message=geovista&color=green&logo=github"></a>
 <br>
-<a href="https://github.com/edsaac/stpyvista-tests"><img alt="stpyvista examples repository" src="https://img.shields.io/static/v1?label=:&message=Examples&color=ff4b4b&logo=github"></a>
-<br>
-<a href="https://stpyvista.streamlit.app"><img alt="Launch stpyvista in Streamlit" src="https://img.shields.io/static/v1?label=:&message=Open%20in%20Streamlit&color=pink&logo=streamlit"></a>
+<a href="https://stpyvista.streamlit.app"><img alt="Launch stpyvista in Streamlit" src="https://img.shields.io/static/v1?label=:&message=More%20examples&color=pink&logo=streamlit"></a>
 </div>
--- planet.py
+++ planet.py
@@ -12,10 +12,10 @@ if "IS_APP_EMBED" not in st.session_state:
 IS_APP_EMBED = st.session_state.IS_APP_EMBED
 
 st.set_page_config(
-    page_title="stpyvista",
+    page_title="stpyvista + geovista",
     page_icon="🧊", 
     layout="wide" if IS_APP_EMBED else "centered", 
-    initial_sidebar_state="collapsed" if IS_APP_EMBED else "expanded")
+    initial_sidebar_state="collapsed")
 
 if "IS_XVFB_RUNNING" not in st.session_state:
     IS_XVFB_RUNNING = is_xvfb()
@@ -32,9 +32,8 @@ with open("assets/style.css") as f:
 
 # Add badges to sidebar
 if not IS_APP_EMBED:
-    with st.sidebar:
-        with open("assets/badges.md") as f:
-            st.markdown(f"""{f.read()}""", unsafe_allow_html=True)
+    with open("assets/badges.md") as f:
+        st.markdown(f"""{f.read()}""", unsafe_allow_html=True)
 
 # *******************************************************************
 @st.cache_resource
@@ -46,26 +45,16 @@ def stpv_planet(dummy:str = "earth"):
 
     blob = gv.Transform.from_1d(
         x,y, data=data, name="Distance to Equator"
-        # radius=gv.common.RADIUS * 1.05
-        # zlevel=1, zscale=0.50
     )
-
-    # if "natural_earth" not in st.session_state:
-    #     # st.session_state.natural_earth = gv.natural_earth_1()
-    #     st.session_state.natural_earth = gv.blue_marble()
-
-    # if "plotter" not in st.session_state:
 
     plotter = gv.GeoPlotter()
     plotter.window_size = [600, 600]
     plotter.set_background("#0e1117")
-    # # Force zlevel alignment of coastlines and base layer.
-    # plotter.add_base_layer(texture=st.session_state.natural_earth, zlevel=0)
     plotter.add_graticule(mesh_args=dict(color="pink", opacity=0.4))
     plotter.add_coastlines(color="white", line_width=8)
     plotter.view_xz(negative=False)
     plotter.add_text(
-        "Earth",
+        "🌎 Earth",
         position="upper_left",
         color="w",
         font_size=18,
@@ -83,18 +72,28 @@ def stpv_planet(dummy:str = "earth"):
 
     return plotter
 
-earth = stpv_planet()
-with st.sidebar:
-    st.title("🧊 `stpyvista`")
-    st.subheader("Show PyVista 3D visualizations in Streamlit")
-    visible_layer = st.sidebar.checkbox("Visible layer?", True)
+cols = st.columns([0.8,2])
 
-earth.actors['blob'].visibility = visible_layer
+with cols[0]:
+    with st.form("Controls"):
+        st.title("🧊 + 🌎")
+        st.header("`stpyvista` + `geovista`")
+        "##### Show PyVista 3D visualizations in Streamlit"
+        "***"
+        visible_layer = st.checkbox("Show layer?", True)
+        btn = st.form_submit_button("**Update**", use_container_width=True)
 
-stpyvista(
-    earth,
-    panel_kwargs=dict(
-        orientation_widget=True, 
-        interactive_orientation_widget=True
-    )
-)
+with cols[1]:
+    container = st.container()
+    earth = stpv_planet()
+    if btn:
+        earth.actors['blob'].visibility = visible_layer
+    
+    with container:
+        stpyvista(
+            earth,
+            panel_kwargs=dict(
+                orientation_widget=True, 
+                interactive_orientation_widget=True
+            )
+        )
